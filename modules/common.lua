6b481a55861cf53695a5df0af0f3e378cd41149f
Jerzy Matlakiewicz <jerzymatlakiewicz@gmail.com>
2024-04-03 19:07:41 +0800

    Introduce finite state machine to the block behaviour

--- modules/common.lua
+++ modules/common.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.TILE_SIZE = 18  -- px
 M.SPAWNPOINT_RAISE = 8  -- tiles
+M.STATES = {current = {still = 1, skidding = 2}, put = 3, falling = 4}
 
 function M.table_has_vec(t, vec)
 	for i = 1,#t do
@@ -12,6 +13,21 @@ function M.table_has_vec(t, vec)
 		end
 	end
 	return false
+end
+
+function M.is_state(my_state, state)
+	if type(state) == "number" then
+		return my_state == state
+	elseif type(state) == "table" then
+		for _, v in pairs(state) do
+			if my_state == v then
+				return true
+			end
+		end
+		return false
+	else
+		print("Wrong state type")
+	end
 end
 
 return M
