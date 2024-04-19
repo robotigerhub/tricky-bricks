49c343d6187edae5fa0528c334afbf63fd92ae9a
Jerzy Matlakiewicz <jerzymatlakiewicz@gmail.com>
2024-04-19 16:56:49 +0200

    Remove unused function

--- modules/common.lua
+++ modules/common.lua
@@ -6,17 +6,6 @@ M.SUPPORT_X_MIN = 4  -- tiles
 M.SUPPORT_X_MAX = 8  -- tiles
 M.STATES = {current = {still = 1, skidding = 2}, put = 3, falling = 4}
 
-function M.table_has_vec(t, vec)
-	for i = 1,#t do
-		if t[i].x == vec.x
-		and t[i].y == vec.y
-		and t[i].z == vec.z then
-			return true
-		end
-	end
-	return false
-end
-
 function M.is_state(my_state, state)
 	if type(state) == "number" then
 		return my_state == state
