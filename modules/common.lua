9935ccb067368d52791d9c3db97dfc8f8f2d5e52
Jerzy Matlakiewicz <jerzymatlakiewicz@gmail.com>
2024-05-18 23:43:47 +0800

    Change block removing logic to fit bigger screens

--- modules/common.lua
+++ modules/common.lua
@@ -7,7 +7,7 @@ M.SUPPORT_X_MIN = 4  -- tiles
 M.SUPPORT_X_MAX = 7  -- tiles
 M.SPAWN_DEADLINE_DIST = 10  -- tiles
 M.SPAWN_RAYCASTER_DIST = 15  -- tiles
-M.STATES = {current = {still = 1, skidding = 2}, put = 3, falling = 4}
+M.STATES = {current = {still = 1, skidding = 2}, put = 3, falling = 4, lost = 5}
 
 function M.is_state(my_state, state)
 	if type(state) == "number" then
